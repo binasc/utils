39579197b509b770295af873345438fa28f47447
binasc <minquan.sun@gmail.com>
2015-02-28 14:51:27 +0800

    update server.sh output info

--- ddns/server.sh
+++ ddns/server.sh
@@ -4,7 +4,6 @@ txt=`nc -4l 0.0.0.0 10053`
 ip=`echo $txt | awk '{print $1}'`
 name=`echo $txt | awk '{print $2}'`
 
-echo $ip
-echo $name
+echo `date` accept from $ip, try to update $name
 
 `pwd`/update-ddns.sh $name $ip
